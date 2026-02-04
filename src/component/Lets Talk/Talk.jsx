43eb9bb2aef960337d6ab2621f364a433350f49a
digvijay sinh rathod <155974214+digvijaysinhx1@users.noreply.github.com>
2026-02-05 00:09:14 +0530

    small changes done & website completed

--- src/component/Lets Talk/Talk.jsx
+++ src/component/Lets Talk/Talk.jsx
@@ -5,7 +5,7 @@ import Link2 from '../btn/Link2'
 
 const Talk = () => {
   return (
-    <div id='Lets-Talk' className='h-80 mt-15 md:h-screen flex flex-col justify-between items-center w-full bg-center bg-no-repeat bg-cover' style={{ backgroundImage: `url(${Bg})` }}>
+    <div id='Lets-Talk' className='h-80 mt-15 md:h-screen flex flex-col justify-between items-center w-full bg-center bg-no-repeat bg-cover' style={{ backgroundImage: `url(${Bg})`, }}>
       <div className="text h-fit flex flex-col items-center font-semibold" style={{fontFamily:"Syne"}}>
         <div className="sub-heading  text-[13px] md:text-3xl lg:text-4xl">
           <h1>Have a project in mind? Just let us know!</h1>
--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -1,6 +1,6 @@
 const Card = (props) => {
   return (
-    <div className='bg-[#0D0D0D] text-[#F1F1F1] hover:bg-[#181818] transition-colors duration-200 ease-in-out h-70  w-70 flex flex-col justify-between items-center rounded-2xl text-center py-10 px-7 md:py-7 md:px-4 gap-3 md:gap-1 border border-[#343434] font-(--font-syne)'>
+    <div className='bg-[#0D0D0D]  text-[#F1F1F1] hover:bg-[#181818] transition-colors duration-200 ease-in-out h-70  w-70 flex flex-col justify-between items-center rounded-2xl text-center py-10 px-7 md:py-7 md:px-4 gap-3 md:gap-1 border border-[#343434] font-(--font-syne)' data-cursor="big">
       <div className="cardPng">
         <img src={props.png} alt="internet png" loading='lazy' className="h-14 w-14"/>
       </div>
