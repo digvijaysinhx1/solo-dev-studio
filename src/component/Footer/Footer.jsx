1889cb9dc42ebc93eee7ae23112a23d060d37330
digvijay sinh rathod <155974214+digvijaysinhx1@users.noreply.github.com>
2026-03-04 16:05:05 +0530

    font adjustments

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='main flex flex-col h-fit items-center mt-15 pb-15 gap-10' style={{fontFamily:"Syne"}}>
         <div className="top" data-cursor="big">
             
-            <h1 className='text-[#E8E4FF] font-semibold text-[96px] w-full md:text-9xl lg:text-[15rem] border-b'>PROVIDENZ</h1>
+            <h1 className='text-[#E8E4FF] font-semibold text-[50px] w-full md:text-9xl lg:text-[14rem] border-b'>PROVIDENZ</h1>
         </div>
         <div className="copy-rights flex w-full justify-around md:font-semibold">
             <span>© 2025  Providenz. All Rights Reserved .</span>
